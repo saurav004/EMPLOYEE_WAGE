e4d63cb45f582295a95deab642ee0fb068f936a9
saurav004 <logtosaurav@gmail.com>
2020-10-09 18:42:00 +0530

    ADDED :Code To Calculate Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
-echo "------------------------- Welcome To Employee Wage Compution ------------------------"
+echo "------------------------- Welcome To Employee Wage Computation ------------------------"
 
-#PRESENT IS SET VALUE 1
+# VARIABLE PRESENT IS CREATED AND SET VALUE = 1
 isPresent=1
 
 #GENERATING RANDOM VALUE TO GET ATTENDANCE
@@ -11,7 +11,16 @@ randomNumber=$((RANDOM%2))
 #TO CHECK THE EMPLOYEE IS PRESENT OR ABSENT
 if [ $randomNumber -eq 	$isPresent ]
 then
-	echo "Employee Present"
+	echo "Employee is Present"
 else
-	echo "Employee Absent"
+	echo "Employee is Absent"
 fi
+empWagePerHour=20
+dailyEmpHrs=8
+#FUNCTION TO CALCULATE EMPLOYEE ONE DAY WAGE
+function calculateDailyEmployeeWage(){
+	calculateEmployeeWage=$(($empWagePerHour*$dailyEmpHrs))
+	echo "Employee's one day wage is equal to" $calculateEmployeeWage "rupees"
+}
+#FUNCTION CALL
+calculateDailyEmployeeWage
