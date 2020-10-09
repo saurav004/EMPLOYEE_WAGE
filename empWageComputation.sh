eaa540c2535dd2e16bb5c61c507ce32e3389da0f
saurav004 <logtosaurav@gmail.com>
2020-10-09 17:18:40 +0530

    ADDED:Code to check Employee Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,17 @@
-#/bin/bash -x
+#!/bin/bash -x
 
 echo "------------------------- Welcome To Employee Wage Compution ------------------------"
+
+#PRESENT IS SET VALUE 1
+isPresent=1
+
+#GENERATING RANDOM VALUE TO GET ATTENDANCE
+randomNumber=$((RANDOM%2))
+
+#TO CHECK THE EMPLOYEE IS PRESENT OR ABSENT
+if [ $randomNumber -eq 	$isPresent ]
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
